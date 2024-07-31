443658e02974d565f56550426c7fa22f938f38c1
Gloman21 <irankundab21ertin@gmail.com>
2024-07-31 12:17:19 +0200

    task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,15 +1,31 @@
-#!/usr/bin/python3
-'''
-Task 1's module.
-'''
+#!/usr/bin/env python3
+"""
+this module has a function that executes coroutines concurrently
+"""
+import asyncio
 from typing import List
-from importlib import import_module as using
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async_generator = using('0-async_generator').async_generator
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    This function waits for a random amount of time between 0 and max_delay
+    and returns the time elapsed
+    Args:
+        n: number of times to call wait_random
+        max_delay: max delay for wait_random
+    Returns:
+        list of delays
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    wait_times = await asyncio.gather(*tasks)
+    return sorted(wait_times)
 
 
-async def async_comprehension() -> List[float]:
-    '''Creates a list of 10 numbers from a 10-number generator.
+async def wait_n1(n: int, max_delay: int) -> List[float]:
+    '''Executes wait_random n times.
     '''
-    return [num async for num in async_generator()]
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
